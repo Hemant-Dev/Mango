28e4957eab0b35ae085a99b18e6e4189057284cc
Hemant Dev <phemant092@gmail.com>
2025-04-17 14:59:14 +0530

    Add login and registration functionality to AuthController
    Updated AuthController to include methods for user login and registration, handling role assignment and validation. Modified BaseService to ensure request data is included only when present. Added RoleAdmin and RoleCustomer constants in SD.cs. Updated Register.cshtml to include a dropdown for role selection during registration.

--- Mango.Web/Controllers/AuthController.cs
+++ Mango.Web/Controllers/AuthController.cs
@@ -1,7 +1,12 @@
-﻿using Mango.Web.Models.Dto;
+﻿using Mango.Web.Models;
+using Mango.Web.Models.Dto;
 using Mango.Web.Service.IService;
+using Mango.Web.Utility;
 using Microsoft.AspNetCore.Identity.Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
 
 namespace Mango.Web.Controllers
 {
@@ -21,11 +26,80 @@ namespace Mango.Web.Controllers
             return View(loginRequestDto);
         }
 
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginRequestDto loginRequestDto)
+        {
+            ResponseDto responseDto = await _authService.LoginAsync(loginRequestDto);
+            if (responseDto.Result != null && responseDto.IsSuccess)
+            {
+                LoginResponseDto loginResponseDto = JsonConvert.DeserializeObject<LoginResponseDto>(Convert.ToString(responseDto.Result));
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ModelState.AddModelError("CustomError", responseDto.Message);
+                return View(loginRequestDto);
+            }
+        }
+
         [HttpGet]
         public IActionResult Register()
         {
+            var roleList = new List<SelectListItem>()
+            {
+                new SelectListItem()
+                {
+                    Text = SD.RoleAdmin,
+                    Value = SD.RoleAdmin
+                },
+                new SelectListItem()
+                {
+                    Text = SD.RoleCustomer,
+                    Value = SD.RoleCustomer
+                }
+            };
+
+            ViewBag.RoleList = roleList;
             return View();
         }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegistrationRequestDto registrationRequestDto)
+        {
+            ResponseDto responseDto = await _authService.RegisterAsync(registrationRequestDto);
+            ResponseDto assignRole;
+            if(responseDto != null && responseDto.IsSuccess)
+            {
+                if (string.IsNullOrEmpty(registrationRequestDto.Role))
+                {
+                    registrationRequestDto.Role = SD.RoleCustomer;
+                }
+                assignRole = await _authService.AssignRoleAsync(registrationRequestDto);
+                if(assignRole != null && assignRole.IsSuccess)
+                {
+                    TempData["success"] = "Registration successful";    
+                    return RedirectToAction(nameof(Login));
+                }
+            }
+            var roleList = new List<SelectListItem>()
+            {
+                new SelectListItem()
+                {
+                    Text = SD.RoleAdmin,
+                    Value = SD.RoleAdmin
+                },
+                new SelectListItem()
+                {
+                    Text = SD.RoleCustomer,
+                    Value = SD.RoleCustomer
+                }
+            };
+
+            ViewBag.RoleList = roleList;
+            return View(registrationRequestDto);
+        }
+
+        
 
         public IActionResult Logout()
         {

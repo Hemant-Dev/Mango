2b6e6787f2b28060770919b079ba327999390019
Hemant Patel <hemant.patel@non.se.com>
2025-04-29 18:01:06 +0530

    Add CartUpsert and RemoveCart endpoints
    This commit introduces two new HTTP POST endpoints: `CartUpsert` for updating or inserting cart data, and `RemoveCart` for removing items from the shopping cart. The `RemoveCart` method also checks if the removed item is the last in the cart, and if so, it removes the associated cart header. Both methods return a `ResponseDto` to indicate the success or failure of the operations.

--- Mango.Services.ShoppingCartAPI/Controllers/CartAPIController.cs
+++ Mango.Services.ShoppingCartAPI/Controllers/CartAPIController.cs
@@ -24,6 +24,7 @@ namespace Mango.Services.ShoppingCartAPI.Controllers
             _mapper = mapper;
         }
 
+        [HttpPost("CartUpsert")]
         public async Task<ResponseDto> CartUpsert(CartDto cartDto)
         {
             try
@@ -72,6 +73,37 @@ namespace Mango.Services.ShoppingCartAPI.Controllers
                 _response.Message = ex.Message.ToString();
                 _response.IsSuccess = false;
             }
+            return _response;
+        }
+
+
+        [HttpPost("RemoveCart")]
+        public async Task<ResponseDto> RemoveCart([FromBody] int cartDetailsId)
+        {
+            try
+            {
+                CartDetails cartDetails = _db.CartDetails
+                    .First(u => u.CartDetailsId == cartDetailsId);
+
+                int totalCountOfCartItem = _db.CartDetails.Where(u => u.CartHeaderId == cartDetails.CartHeaderId).Count();
+                _db.CartDetails.Remove(cartDetails);
+                if(totalCountOfCartItem == 1)
+                {
+                    var cartHeaderToRemove = await _db.CartHeaders
+                        .FirstOrDefaultAsync(u => u.CartHeaderId == cartDetails.CartHeaderId);
+
+                    _db.CartHeaders.Remove(cartHeaderToRemove);
+                }
+                await _db.SaveChangesAsync();
+
+                _response.Result = true;
+            }
+            catch (Exception ex)
+            {
+                _response.Message = ex.Message.ToString();
+                _response.IsSuccess = false;
+            }
+
             return _response;
         }
 

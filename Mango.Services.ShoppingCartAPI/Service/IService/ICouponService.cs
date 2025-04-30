d3ca2c7779b381b5882ac5fb3a7dd2ec309ced93
Hemant Patel <hemant.patel@non.se.com>
2025-04-30 18:17:24 +0530

    Update Coupon API integration and service implementation
    - Commented out the [Authorize] attribute in CouponAPIController.cs.
    - Added CouponAPI URL to appsettings.json for service connectivity.
    - Created CouponService.cs to implement ICouponService for fetching coupon details.
    - Defined ICouponService interface in ICouponService.cs with GetCoupon method.

---
+++ Mango.Services.ShoppingCartAPI/Service/IService/ICouponService.cs
@@ -0,0 +1,10 @@
+﻿using Mango.Services.ShoppingCartAPI.Models.Dto;
+using System.Runtime.Serialization;
+
+namespace Mango.Services.ShoppingCartAPI.Service.IService
+{
+    public interface ICouponService
+    {
+        Task<CouponDto> GetCoupon(string couponCode);
+    }
+}
